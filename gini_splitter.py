754034852b1af1150c47e7f39a60f4dde1ced5f3
David Thaler <davidthaler@gmail.com>
2017-08-11 19:11:25 -0700

    initial commit of gini_splitter

---
+++ gini_splitter.py
@@ -0,0 +1,56 @@
+import numpy as np
+from collections import Counter
+
+
+def split(x, y):
+    '''
+    Given features x and labels y, find the feature index and threshold for a
+    split that produces the largest reduction in Gini impurity.
+
+    Args:
+        x: m x n numpy array of numeric features
+        y: m-element 1-D numpy array of labels; must be 0-1.
+
+    Returns:
+        2-tuple of feature index and split threshold.
+    '''
+    m, n = x.shape
+    best_feature = -1
+    best_thr = 0.0
+    best_score = 0.5
+    # Iterate over features of x
+    for j in range(n):
+        f = x[:, j]
+        cts = Counter()
+        pos = Counter()
+        neg = Counter()
+        # get totals, positives and negatives for each *unique* value in f
+        for k in range(m):
+            cts[f[k]] += 1
+            pos[f[k]] += y[k]
+            neg[f[k]] += (1 - y[k])
+        # We start with all data on the right
+        n_left = 0                          # number of data points on left branch
+        n_right = 0                          # number of data points on right branch
+        npos_left = 0                   # number of positives on left
+        nneg_left = 0                   # number of positives on left
+        npos_right = y.sum()            # number of positives on right
+        nneg_right = n_right - npos_right    # number of negatives on right
+        g = np.sort(np.unique(f))
+        # range(len(g) - 1) omits the split with an empty right branch
+        for k in range(len(g) - 1):
+            val = g[k]
+            n_left += cts[val]
+            n_right -= cts[val]
+            npos_left += pos[val]
+            npos_right -= pos[val]
+            nneg_left += neg[val]
+            nneg_right -= neg[val]
+            gini_left  = 1 - (npos_left/n_left)**2 - (nneg_left/n_left)**2
+            gini_right = 1 - (npos_right/n_right)**2 - (nneg_right/n_right)**2
+            gini_split = (n_left/m) * gini_left + (n_right/m) * gini_right
+            if gini_split < best_score:
+                best_feature = j
+                best_thr = g[k]
+                best_score = gini_split
+    return (best_feature, best_thr, best_score)
