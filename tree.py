7b74b027be39a5f36b2ad1139cc8dd8e6f9621fa
David Thaler <davidthaler@gmail.com>
2017-08-15 15:35:36 -0700

    simplified some of tree.build.tree

--- tree.py
+++ tree.py
@@ -21,13 +21,8 @@ def build_tree(x, y):
     if pos == 0 or pos == ct:
         return np.array([[feature, thr, ct, pos]])
     feature, thr, _ = split(x, y)
-    left_mask = x[:, feature] <= thr
-    x_left = x[left_mask]
-    y_left = y[left_mask]
-    left_tree = build_tree(x_left, y_left)
-    right_mask = x[:, feature] > thr
-    x_right = x[right_mask]
-    y_right = y[right_mask]
-    right_tree = build_tree(x_right, y_right)
+    mask = x[:, feature] <= thr
+    left_tree = build_tree(x[mask], y[mask])
+    right_tree = build_tree(x[~mask], y[~mask])
     root = np.array([[feature, thr, ct, pos]])
     return np.concatenate([root, left_tree, right_tree])
