35e9b480a7f9d54cd6bdef8a1fe8e45b1f2485a3
David Thaler <davidthaler@gmail.com>
2017-08-26 20:04:40 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='simple_tree',
+    version='0.1.0',
+    url='https://github.com/davidthaler/SimpleTree',
+    author='David Thaler',
+    license='MIT',
+    packages=['simple_tree', 'simple_tree.tests']
+)
