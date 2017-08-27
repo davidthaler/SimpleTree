6658e1781af4ea43eae5297fd14b8cb4eece2fe1
David Thaler <davidthaler@gmail.com>
2017-08-27 00:28:59 -0700

    added description to setup

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import setup
 setup(
     name='simple_tree',
     version='0.1.0',
+    description='An all-python decision tree with key sections accelerated using Numba.',
     url='https://github.com/davidthaler/SimpleTree',
     author='David Thaler',
+    author_email='davidthaler@gmail.com',
     license='MIT',
     packages=['simple_tree', 'simple_tree.tests']
 )
