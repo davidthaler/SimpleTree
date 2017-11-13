ea738d3bb1fa4ca919b5c5f123e3366b95b311db
David Thaler <davidthaler@gmail.com>
2017-11-13 15:56:38 -0800

    Added random forest regressor/classifier. Smoke tested ok. Added one example.

---
+++ examples/rf_spam_example.py
@@ -0,0 +1,46 @@
+'''
+This example compares sklearn.ensemble.RandomForestClassifier 
+to simple_tree.simple_forest.RFClassifier for accuracy and
+running time. The accuracy is close. The running time is not.
+'''
+from time import time
+from sklearn.ensemble import RandomForestClassifier
+from simple_tree.simple_forest import RFClassifier
+from simple_tree.datasets import load_spam
+from sklearn.metrics import confusion_matrix as cm
+
+print(__doc__)
+
+# Set-up
+xtr, ytr, xte, yte = load_spam()
+rf = RandomForestClassifier(n_estimators=30, min_samples_leaf=30, max_depth=5)
+myrf = RFClassifier(n_trees=30, min_samples_leaf=30, max_depth=5)
+
+# Running Time
+print('Running time on fit')
+print('-------------------')
+print('Fitting on sklearn.RandomForestRegressor')
+start = time()
+rf.fit(xtr, ytr)
+end = time()
+et = end - start
+print('Fitting complete.')
+print('Elapsed time %.6f seconds\n' % et)
+
+print('Fitting on simple_tree.simple_forest.RFClassifier')
+start = time()
+myrf.fit(xtr, ytr)
+end = time()
+et = end - start
+print('Fitting complete.')
+print('Elapsed time %.6f seconds\n' % et)
+
+# Accuracy
+print('Accuracy - Confusion Matrices')
+print('-----------------------------')
+print('For sklearn.RandomForestRegressor:')
+print(cm(yte, rf.predict(xte)))
+print()
+print('For RFClassifier:')
+print(cm(yte, myrf.predict(xte)))
+print()
---
+++ simple_tree/simple_forest.py
@@ -0,0 +1,168 @@
+'''
+Module simple_forest implements random forest models for regression and 
+binary classification using decision trees from simple_tree.
+Class SimpleForest is a base class for regression and classification models.
+Class RFClassifier is a concrete class for binary classification.
+Class RFRegressor is an RF regression model.
+
+author: David Thaler
+date: Novermber 2017
+'''
+import numpy as np
+from . import RegressionTree, ClassificationTree
+
+class SimpleForest():
+    '''
+    SimpleForest is a base class for random forest models based 
+    on the trees in simple_tree.
+
+    Args:
+        n_trees: number of trees to use in model
+        other: same as underlying tree type
+    '''
+    def __init__(self, n_trees, min_samples_leaf=1, max_features=None,
+                    max_depth=None):
+        self.n_trees = n_trees
+        self.min_samples_leaf = min_samples_leaf
+        self.max_features = max_features
+        self.max_depth = max_depth
+
+    def fit(self, x, y):
+        '''
+        Fits a random forest model using the base estimator from a subclass. 
+        Also sets the oob_decision_function_ attribute.
+
+        Args:
+            x: Training data features; ndarray of shape (n_samples, n_features)
+            y: Training set labels; shape is (n_samples, )
+
+        Returns:
+            Returns self, the fitted estimator
+        '''
+        m, n = x.shape
+        self.estimators_ = []
+        all_idx = np.arange(m)
+        oob_ct = np.zeros(m)
+        oob_tot_dv = np.zeros(m)
+        for k in range(self.n_trees):
+            model = self._get_base_estimator(n)
+            boot_idx = np.random.randint(m, size=m)
+            oob_idx = np.setdiff1d(all_idx, boot_idx)
+            model.fit(x[boot_idx], y[boot_idx])
+            self.estimators_.append(model)
+            oob_ct[oob_idx] += 1
+            oob_tot_dv[oob_idx] += model.decision_function(x[oob_idx])
+        self.oob_decision_function = oob_tot_dv / oob_ct
+        return self
+
+    def decision_function(self, x):
+        '''
+        Returns the decision function for each row in x.
+        For regression models, this is the prediction.
+        For classifcation models, it is the class 1 probability.
+        In either case, it is the average over the trees in this forest.
+
+        Args:
+            x: Test data to predict; ndarray of shape (n_samples, n_features)
+
+        Returns:
+            array (n_samples,) decision function for each row in x
+        '''
+        dv = np.zeros(len(x))
+        for model in self.estimators_:
+            dv += model.decision_function(x)
+        return dv / self.n_trees
+
+    def __repr__(self):
+        '''Return a string representation of this model'''
+        name = self.__class__.__name__
+        return ('%s(n_trees=%s, min_samples_leaf=%s, max_features=%s, max_depth=%s)' %
+                (name, self.n_trees, self.min_samples_leaf,
+                    self.max_features, self.max_depth))
+
+
+class RFClassifier(SimpleForest):
+    '''
+    Concrete class extending SimpleForest as a classification model.
+
+    Args:
+        see SimpleForest
+    '''
+
+    def _get_base_estimator(self, n_features):
+        '''
+        Return configured decision tree classifier.
+        Default for max_features is sqrt(number of features)
+
+        Args:
+            n_features: number of columns in x (x.shape[1])
+
+        Returns:
+            Configured ClassifcationTree for this RF model.
+        '''
+        max_features = (int(np.ceil(np.sqrt(n_features)))
+                        if self.max_features is None else self.max_features)
+        return ClassificationTree(min_samples_leaf=self.min_samples_leaf,
+                                  max_features=max_features,
+                                  max_depth=self.max_depth)
+
+    def predict_proba(self, x):
+        '''
+        Predicts probabilities of the positive class for each row in x.
+
+        Args:
+            x: Test data to predict; ndarray of shape (n_samples, n_features)
+
+        Returns:
+            array of shape (n_samples,) of probabilities for class 1.
+        '''
+        return self.decision_function(x)
+
+    def predict(self, x):
+        '''
+        Predicts class membership for the rows in x. 
+
+        Args:
+            x: Test data to predict; ndarray of shape (n_samples, n_features)
+
+        Returns:
+            array of shape (n_samples, ) of class labels for each row
+        '''
+        return (self.predict_proba(x) > 0.5).astype(int)
+
+
+class RFRegressor(SimpleForest):
+    '''
+    Concrete class extending SimpleForest as a regression model.
+
+    Args:
+        see SimpleForest
+    '''
+    def _get_base_estimator(self, n_features):
+        '''
+        Return configured decision tree regressor.
+        Default for max_features is (number of features) / 3
+
+        Args:
+            n_features: number of columns in x (x.shape[1])
+
+        Returns:
+            Configured RegressionTree for this RF model.
+        '''
+        max_features = (int(np.ceil(n_features / 3))
+                        if self.max_features is None else self.max_features)
+        return RegressionTree(min_samples_leaf=self.min_samples_leaf,
+                              max_features=max_features,
+                              max_depth=self.max_depth)
+
+    def predict(self, x):
+        '''
+        Estimates target for each row in x.
+
+        Args:
+            x: Test data to predict; ndarray of shape (n_samples, n_features)
+
+        Returns:
+            array (n_samples,) of estimates of target for each row in x
+        '''
+        return self.decision_function(x)
